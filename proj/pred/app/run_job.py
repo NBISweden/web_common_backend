9fb795a5da97af1b627ed19e91903635c94a9063
nanjiangshu <nanjiang.shu@gmail.com>
2017-12-20 11:05:13 +0100

    update docker_subcons command line

--- proj/pred/app/run_job.py
+++ proj/pred/app/run_job.py
@@ -131,9 +131,9 @@ def GetCommand(name_software, seqfile_this_seq, tmp_outpath_result, tmp_outpath_
                 "-verbose"]
     elif name_software in ['docker_subcons']:
         containerID = 'subcons'
-        cmd =  ["/usr/bin/docker", "exec", containerID, 
+        cmd =  ["/usr/bin/docker", "exec", "--user", "user", containerID, 
                 "script", "/dev/null", "-c", 
-                "cd %s; /home/app/subcons/master_subcons.sh %s %s"%(
+                "cd %s; /app/subcons/master_subcons.sh %s %s"%(
                     docker_tmp_outpath_result, docker_seqfile_this_seq,
                     docker_tmp_outpath_this_seq)]
     elif name_software in ['prodres']:#{{{
