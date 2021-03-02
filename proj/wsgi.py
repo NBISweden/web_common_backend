5be1ab733963fe7db5571a0c4266b5619ab3ca3d
nanjiangshu <nanjiang.shu@gmail.com>
2021-03-02 23:22:19 +0100

    clean wsgi

--- proj/wsgi.py
+++ proj/wsgi.py
@@ -18,10 +18,8 @@ path_log = "%s/pred/static/log"%(rundir)
 # Activate the virtual env
 activate_env="%s/env/bin/activate_this.py"%(basedir)
 exec(compile(open(activate_env, "r").read(), activate_env, 'exec'), dict(__file__=activate_env))
-os.system("echo which python; which python >> %s/debug.log" %( path_log) )
 
 #Add the site-packages of the virtualenv
-site.addsitedir("%s/env/lib/python3.7/site-packages/"%(basedir))
 
 # Add the directory for the project
 sys.path.append(basedir)
