10e3b2517433a8f30214eca1ee893ddaa0d832ad
nanjiangshu <nanjiang.shu@gmail.com>
2021-03-08 21:42:19 +0100

    clean wsgi

--- proj/wsgi.py
+++ proj/wsgi.py
@@ -15,16 +15,16 @@ rundir = os.path.dirname(os.path.abspath(__file__))
 basedir = os.path.abspath("%s/../"%(rundir))
 path_log = "%s/pred/static/log"%(rundir)
 
+
 # Activate the virtual env
 activate_env="%s/env/bin/activate_this.py"%(basedir)
 exec(compile(open(activate_env, "r").read(), activate_env, 'exec'), dict(__file__=activate_env))
+
 
 #Add the site-packages of the virtualenv
 
 # Add the directory for the project
 sys.path.append(basedir)
-#sys.path.insert(0,"%s/env/bin"%(basedir))
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 from django.core.wsgi import get_wsgi_application
